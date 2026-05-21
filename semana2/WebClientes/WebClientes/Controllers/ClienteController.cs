e48e45f203f9e47731f0319a2d0014e4396881b5
megajavi27 <javinoboa@gmail.com>
2026-05-21 17:06:56 -0500

    semana2
    semana2

---
+++ semana2/WebClientes/WebClientes/Controllers/ClienteController.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebClientes.Models;
+
+namespace WebClientes.Controllers
+{
+    public class ClienteController : Controller
+    {
+        List<ClienteModel> _lista_Clientes = new List<ClienteModel>() {
+            new ClienteModel{
+                id = 1,
+                Nombres_Cliente="Luis Antonio",
+                Apellidos = "Llerena Ocaña",
+                Direccion = "Ambato",
+                Telefono = "0987654321",
+                Correo = "lleroc1@gmail.com"
+            },
+            new ClienteModel{
+                id = 2,
+                Nombres_Cliente="Otro Luis",
+                Apellidos = "Otro Llerena",
+                Direccion = "Quero",
+                Telefono = "0999999999",
+                Correo = "otro@gmail.com"
+            }
+        };
+
+        // GET: ClienteController
+        public ActionResult Index()
+        {
+            return View(_lista_Clientes);
+        }
+
+        // GET: ClienteController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: ClienteController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ClienteController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ClienteController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: ClienteController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ClienteController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: ClienteController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ semana2/WebClientes/WebClientes/Models/ClienteModel.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebClientes.Models
+{
+    public class ClienteModel
+    {
+        //[key]     para definir la clave primaria
+        public int id { get; set; }
+
+        [Display(Name = "Nombres del Cliente")]
+        [MaxLength(100, ErrorMessage = "El maximo numero de caracteres es 100")]
+        [Required(ErrorMessage = "El campo es requerido")]
+        public string Nombres_Cliente { get; set; }
+
+        [MaxLength(100, ErrorMessage = "El maximo numero de caracteres es 100")]
+        [Required(ErrorMessage = "El campo es requerido")]
+        public string Apellidos { get; set; }
+
+        [Required(ErrorMessage = "El campo es requerido")]
+        public string Direccion { get; set; }
+
+        [Required(ErrorMessage = "El campo es requerido")]
+        [MaxLength(17, ErrorMessage = "El máximo número de caracteres es 17")]
+        [MinLength(9, ErrorMessage = "El minimo de caracteres es 9")]
+        public string Telefono { get; set; }
+
+        [EmailAddress(ErrorMessage = "El correo electrónico no es válido")]
+        public string Correo { get; set; }
+    }
+}
